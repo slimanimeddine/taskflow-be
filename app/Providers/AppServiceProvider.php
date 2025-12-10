b43c8c5c00cc482e0727e33cf2da46a67dcc87d2
Imed Eddine Abderrahmane SLIMANI <imeds2000@gmail.com>
2025-12-10 19:00:24 +0100

    update deps, add some good defaults to appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,15 @@
 namespace App\Providers;
 
 use App\Traits\ApiResponses;
+use Carbon\CarbonImmutable;
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
@@ -23,6 +29,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::automaticallyEagerLoadRelationships();
+
+        Model::shouldBeStrict();
+
+        Model::unguard();
+
+        Date::use(CarbonImmutable::class);
+
+        if (App::environment('production')) {
+            URL::forceHttps();
+        }
+
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
         // rate limiting
         RateLimiter::for('sign-in', fn (Request $request) => [
             Limit::perMinute(5)
